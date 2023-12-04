af8450acc0cfcf05a6efb0af5a0852ef4ef76d93
yuetong3yu <yuetong3yu@gmail.com>
2023-12-04 14:00:28 +1100

    day 1 code challenge: finish

--- day1/index.js
+++ day1/index.js
@@ -1,15 +1,30 @@
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 let input = ''
 
-fs.readFile(path.join(__dirname, './input.txt'), (err, data) => {
+fs.readFile(path.join(__dirname, './input2.txt'), {}, (err, data) => {
   input += data.toString()
   const datas = input.split('\n')
+
+  let sum = 0
+  for (let i = 0; i < datas.length; i++) {
+    const num = getNumbersAtBothEnds(datas[i])
+    sum += num
+  }
+  console.log(sum)
 })
 
 export function getNumbersAtBothEnds(str) {
   const numberRegex = /^\d+$/
   let l = 0,
     r = str.length - 1
+  while (!numberRegex.test(str[l]) && l < r) l++
+  while (!numberRegex.test(str[r]) && l < r) r--
+
+  const num = +`${str[l]}${str[r]}`
+
+  return Number.isNaN(num) ? 0 : num
 }
