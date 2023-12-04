1ee45affe8539155ade776df7a3ef09bbfac7742
yuetong3yu <yuetong3yu@gmail.com>
2023-12-04 22:15:27 +1100

    day3 challenge: create files

---
+++ day3/index.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+let input = ''
+
+fs.readFile(path.join(__dirname, './input.txt'), {}, (err, data) => {
+  input += data.toString()
+  console.log(input)
+})
