d9668e3f9453f6dd300537eb4a97ad2bea279ca3
yuetong3yu <yuetong3yu@gmail.com>
2023-12-04 13:12:59 +1100

    day 1 unit test: core function

--- day1/index.test.js
+++ day1/index.test.js
@@ -1,7 +1,19 @@
 import { getNumbersAtBothEnds } from './index'
 
 describe('getNumbersAtBothEnds', () => {
-  test('test', () => {
-    expect(1).toBe(1)
+  test('should get correct number when only 2 digits in string', () => {
+    expect(getNumbersAtBothEnds('aldfk5jl4sadlf')).toBe(54)
+  })
+
+  test('should get 0 when only no digit in string', () => {
+    expect(getNumbersAtBothEnds('alsdkfjl')).toBe(0)
+  })
+
+  test('should get repeated number when only 1 digit in string', () => {
+    expect(getNumbersAtBothEnds('asod8ufy')).toBe(88)
+  })
+
+  test('should get the numbers at both ends when multiple digits in the string', () => {
+    expect(getNumbersAtBothEnds('asd3lk4fj2ad2sl9lk')).toBe(39)
   })
 })
