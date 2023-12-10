66a6b2be37dc52667fc3ca0c6610c50ada965854
yuetong3yu <yuetong3yu@gmail.com>
2023-12-10 18:37:01 +1100

    day 10: fuck, can't solve it

--- day10/index.js
+++ day10/index.js
@@ -3,26 +3,38 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-import { getStartPoint } from './utils.js'
+import { getStartPoint, getResultFromStartPoint } from './utils.js'
 
 let input = ''
 
-fs.readFile(path.join(__dirname, './sample.txt'), {}, (err, data) => {
+fs.readFile(path.join(__dirname, './input.txt'), {}, (err, data) => {
   input += data.toString()
   const strings = input.split('\n')
 
   const start_point = getStartPoint(strings)
+  console.log(start_point)
   // the result can be either [] or undefined
   // go top
-  const top_result = getResultFromStartPoint(strings, start_point, 'top')
-  // go left
-  const left_result = getResultFromStartPoint(strings, start_point, 'left')
-  // go bottom
-  const bottom_result = getResultFromStartPoint(strings, start_point, 'bottom')
-  // go top
-  const right_result = getResultFromStartPoint(strings, start_point, 'right')
-  if (top_result) return top_result.length / 2
-  if (left_result) return left_result.length / 2
-  if (bottom_result) return bottom_result.length / 2
-  if (right_result) return right_result.length / 2
+  // const top_result = getResultFromStartPoint(strings, start_point, 'top', [
+  //   start_point,
+  // ])
+  // // go left
+  // const left_result = getResultFromStartPoint(strings, start_point, 'left', [
+  //   start_point,
+  // ])
+  // // go bottom
+  // const bottom_result = getResultFromStartPoint(
+  //   strings,
+  //   start_point,
+  //   'bottom',
+  //   [start_point]
+  // )
+  // // go top
+  // const right_result = getResultFromStartPoint(strings, start_point, 'right', [
+  //   start_point,
+  // ])
+  // if (top_result) return top_result.length / 2
+  // if (left_result) return left_result.length / 2
+  // if (bottom_result) return bottom_result.length / 2
+  // if (right_result) console.log(right_result.length / 2)
 })
--- day10/utils.js
+++ day10/utils.js
@@ -1,5 +1,36 @@
 const START_POINT = 'S'
 
+// const CONNECTABLE_MAP = {
+//   ['|']: ['F', 'L', '7', 'J'],
+//   // ['-']: ['F', '7', 'L', 'J'],
+//   ['-']: {
+//     right: ['']
+//   },
+//   ['7']: {
+//     top: '-',
+//     right: '|',
+//   },
+//   ['J']: {
+//     bottom: '-',
+//     right: '|',
+//   },
+//   ['L']: {
+//     bottom: '-',
+//     left: '|',
+//   },
+//   ['F']: {
+//     top: '-',
+//     left: '|',
+//   },
+//   ['.']: undefined,
+// }
+const DIRECTION_TO_ALLOWABLE_JOINT = {
+  top: ['|', '7', 'F'],
+  bottom: ['|', 'J', 'L'],
+  left: ['-', 'L', 'F'],
+  right: ['-', 'J', '7'],
+}
+
 export function getStartPoint(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
@@ -8,7 +39,87 @@ export function getStartPoint(matrix) {
   }
 }
 
-export function getResultFromStartPoint(matrix, point, direction_from) {}
+export function getResultFromStartPoint(
+  matrix,
+  start_point,
+  direction_from,
+  res
+) {
+  const target_point = getTargetPoint(matrix, start_point, direction_from)
+  // IF loop founded
+  if (target_point === 'S') {
+    return res
+  }
+  // IF not jointed
+  if (!DIRECTION_TO_ALLOWABLE_JOINT[direction_from]?.includes(target_point)) {
+    return undefined
+  }
+
+  if (direction_from === 'top') {
+    const target_point = [start_point[0] - 1, start_point[1]]
+    res.push(target_point)
+    const next_char = matrix[target_point[0]][target_point[1]]
+    let direction = ''
+    if (next_char === '|') {
+      direction = 'top'
+    } else if (next_char === '7') {
+      direction = 'left'
+    } else if (next_char === 'F') {
+      direction = 'right'
+    } else {
+      return undefined
+    }
+    return getResultFromStartPoint(matrix, target_point, direction, res)
+  }
+  if (direction_from === 'left') {
+    const target_point = [start_point[0], start_point[1] - 1]
+    res.push(target_point)
+    const next_char = matrix[target_point[0]][target_point[1]]
+    let direction = ''
+    if (next_char === '-') {
+      direction = 'left'
+    } else if (next_char === 'L') {
+      direction = 'top'
+    } else if (next_char === 'F') {
+      direction = 'bottom'
+    } else {
+      return undefined
+    }
+    return getResultFromStartPoint(matrix, target_point, direction, res)
+  }
+  if (direction_from === 'right') {
+    const target_point = [start_point[0], start_point[1] + 1]
+    res.push(target_point)
+    const next_char = matrix[target_point[0]][target_point[1]]
+    let direction = ''
+    if (next_char === '-') {
+      direction = 'right'
+    } else if (next_char === 'J') {
+      direction = 'top'
+    } else if (next_char === '7') {
+      direction = 'bottom'
+    } else {
+      return undefined
+    }
+    return getResultFromStartPoint(matrix, target_point, direction, res)
+  }
+  if (direction_from === 'bottom') {
+    const target_point = [start_point[0] + 1, start_point[1]]
+    res.push(target_point)
+    const next_char = matrix[target_point[0]][target_point[1]]
+    let direction = ''
+    if (next_char === '|') {
+      direction = 'bottom'
+    } else if (next_char === 'J') {
+      direction = 'left'
+    } else if (next_char === 'L') {
+      direction = 'right'
+    } else {
+      return undefined
+    }
+    return getResultFromStartPoint(matrix, target_point, direction, res)
+  }
+}
 
 export function isThisPointConnected(matrix, point, from) {}
 
